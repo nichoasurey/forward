18864de374b1abda7c2fc6e9665faeb209d26c94
silence <nicholasurey@gmail.com>
2020-05-21 17:40:50 +0800

    update

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express=require('express');
 
-const app = express();
-const config = require('config');
-const moment = require('moment');
+const app                    =express();
+const config                 =require('config');
+const moment                 =require('moment');
+//const ProxyAgent             =require('proxy-agent');
+const {createProxyMiddleware}=require('http-proxy-middleware');
 
 //////////////////////////////////////////////////////
 // program start
@@ -11,22 +13,42 @@ const moment = require('moment');
 console.log('===================================');
 console.log('|        all service init         |');
 console.log('===================================');
-console.log("NODE_ENV : ", process.env.NODE_ENV);
+console.log('NODE_ENV : ',process.env.NODE_ENV);
 
-app.get('/hello', function (req, res) {
+app.get('/hello',function(req,res){
   //res.header('Access-Control-Allow-Credentials', 'true');
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-let port = config.get("port");
-app.listen(port, function () {
+let options={
+  'target':'http://127.0.0.1:5021', // 目标服务器 host
+  //'agent': new ProxyAgent('socks5://127.0.0.1:5021'),
+  'changeOrigin':true,               // 默认false，是否需要改变原始主机头为目标URL
+  'ws':true,                         // 是否代理websockets
+  'pathRewrite':{
+    //"^/api/old-path" : "/api/new-path",     // 重写请求，比如我们源访问的是api/old-path，那么请求会被解析为/api/new-path
+    //"^/api/remove/path" : "/path"           // 同上
+  },
+  'router':{
+    // 如果请求主机 == "dev.localhost:3000",
+    // 重写目标服务器 "http://www.example.org" 为 "http://localhost:8000"
+    //"dev.localhost:3000" : "http://localhost:8000"
+  }
+};
+
+let proxy=createProxyMiddleware(options);
+
+app.use('/',proxy);
+
+let port=config.get('port');
+app.listen(port,function(){
   console.log('===================================');
   console.log('|   app listening on port '+port+'!   |');
-  console.log('|  ',moment().format('YYMMDD HH:mm:ss'),'           |');
-  console.log('===================================')
-})
+  console.log('|  ',moment().format('YYMMDD HH:mm:ss'),'              |');
+  console.log('===================================');
+});
 
-function graceful() {
+function graceful(){
 
   console.log('graceful');
   console.log('bye');
@@ -34,8 +56,8 @@ function graceful() {
 
 }
 
-process.on('SIGTERM', graceful);
-process.on('SIGINT' , graceful);
+process.on('SIGTERM',graceful);
+process.on('SIGINT',graceful);
 
 
 
